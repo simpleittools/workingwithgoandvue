0793052ad6296287d48e3b10904707811ad0dce6
icedvigilante <wm.ryan.mooney@gmail.com>
2022-10-24 16:32:03 -0800

    Create the cookie on login for the front end.

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -11,6 +11,13 @@ import HeaderComponent from "@/components/HeaderComponent";
 import FooterComponent from "@/components/FooterComponent";
 import { store } from "@/components/store";
 
+const getCookie = (name) => {
+  return document.cookie.split("; ").reduce((r, v) => {
+    const parts = v.split("=");
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+  }, "");
+}
+
 export default {
   name: 'App',
   components: {
@@ -21,6 +28,23 @@ export default {
     return {
       store
     }
+  }, beforeMount() {
+    // check for a cookie
+    let data = getCookie("_site_data")
+
+    if (data !== "") {
+      let cookieData = JSON.parse(data)
+
+      // update store
+      store.token = cookieData.token.token;
+      store.user = {
+        id: cookieData.user.id,
+        first_name: cookieData.user.first_name,
+        last_name: cookieData.user.last_name,
+        email: cookieData.user.email,
+      }
+    }
+
   }
 }
 </script>
