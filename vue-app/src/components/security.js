cab84d81b56cb57f6ca1bd5d7fb5476b31785c93
icedvigilante <wm.ryan.mooney@gmail.com>
2022-10-26 16:05:51 -0800

    add composable for the security authentication

---
+++ vue-app/src/components/security.js
@@ -0,0 +1,28 @@
+import {store} from "@/components/store";
+import router from "@/router";
+
+let security = {
+  // make sure user is authenticated
+  requireToken: function() {
+    if (store.token === "") {
+      router.push({name: "Login"})
+      return false
+    }
+  },
+
+  // create request options and send them back
+  requestOptions: function(payload) {
+    const headers = new Headers()
+    headers.append("Content-Type", "application/json")
+    headers.append("Authorization", "Bearer " + store.token)
+
+    return {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: headers,
+    }
+
+    }
+}
+
+export default Security
